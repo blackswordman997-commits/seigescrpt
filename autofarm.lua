009f784ba775e59ae91893298d56614e4fc4b8be
blackswordman997-commits <blackswordman997@gmail.com>
2025-09-27 17:22:03 +0100

    Create autofarm.lua

---
+++ autofarm.lua
@@ -0,0 +1,4 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("ASH X HUB", "BloodTheme")
+local Tab = Window:NewTab("TELEPORTER")
+local Section = Tab:NewSection("WORLDS")
